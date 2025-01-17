a46f17f63a47d1c41ffb5e326d7e185696723051
Liwei-Ji <jpw0616@gmail.com>
2025-01-17 23:17:34 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css';
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
+import Info from './components/Info'
 
 export default function Home() {
- return <div>
-   <h1>Hello</h1>
- </div>
+  return (
+    <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+      <Navbar />
+      <Hero />
+      <Projects />
+      <Info />
+    </div>
+  )
 }
+
